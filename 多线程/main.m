bcc96e48b8df7f9fb1377f2fbb1e5799335e00e3
ysx <ysx@192.168.1.8>
2021-03-13 15:45:48 +0800

    Initial Commit

---
+++ 多线程/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  多线程
+//
+//  Created by ysx on 2021/3/13.
+//  Copyright © 2021 ysx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
